00e6708086ddbad07532cc26149c0757742b1a40
M Bainandhika Baghaskara P <bainandhika.baghaskara@dansmultipro.com>
2026-06-10 11:52:35 +0700

    chore: verify main application entry point

--- kafka-redis-demo/src/main/java/com/baghaskara/kafka_redis_demo/KafkaRedisDemoApplication.java
+++ kafka-redis-demo/src/main/java/com/baghaskara/kafka_redis_demo/KafkaRedisDemoApplication.java
@@ -3,11 +3,13 @@ package com.baghaskara.kafka_redis_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @SpringBootApplication = @Configuration + @EnableAutoConfiguration + @ComponentScan
 @SpringBootApplication
 public class KafkaRedisDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaRedisDemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        // Bootstraps the Spring ApplicationContext.
+        // Equivalent to setting up router and http.ListenAndServe(":8080", router) in Go.
+        SpringApplication.run(KafkaRedisDemoApplication.class, args);
+    }
 }

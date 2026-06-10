2dcfdfc9f677c1d90ec574548a6061d5e6d7a6fc
M Bainandhika Baghaskara P <bainandhika.baghaskara@dansmultipro.com>
2026-06-10 11:42:07 +0700

    chore: initialize spring boot project and add postgres docker-compose

---
+++ kafka-redis-demo/src/main/java/com/baghaskara/kafka_redis_demo/KafkaRedisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.baghaskara.kafka_redis_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRedisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRedisDemoApplication.class, args);
+	}
+
+}
---
+++ kafka-redis-demo/src/test/java/com/baghaskara/kafka_redis_demo/KafkaRedisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baghaskara.kafka_redis_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRedisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
